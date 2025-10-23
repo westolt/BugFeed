ab69a02c8fe7a573207e73b7977508c3962fa096
westolt <westolt@ad.helsinki.fi>
2025-10-23 21:11:01 +0300

    Add FeedItem class to models

--- bugfeed/models.py
+++ bugfeed/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class FeedItem(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    pub_date = models.DateTimeField('date published')
+    likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)
+
+    def __str__(self):
+        return f'{self.owner.username}: {self.content}'
