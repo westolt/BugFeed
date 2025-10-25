7fe37dadb817df3aea114f9d66220cff459f876e
westolt <westolt@ad.helsinki.fi>
2025-10-25 23:16:46 +0300

    Add attack folder and flaw3 screenshots

--- mysite/settings.py
+++ mysite/settings.py
@@ -46,7 +46,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     # FIX: add this middleware
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
